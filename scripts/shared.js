aa095b9c073033de2b96ff881c5d22ee5266317b
Wusabinga <s.loecher.slc@gmail.com>
2020-10-19 10:31:01 +0200

    Introduced drop down menu

--- scripts/shared.js
+++ scripts/shared.js
@@ -1,8 +1,15 @@
-const mobileNavBtn = document.querySelector(".mobile-nav");
-const mobileMainNav = document.querySelector(".mobile-main-nav");
+const mobileNavBtn = document.querySelector('.fa-bars');
+const mobileMainNav = document.querySelector('.mobile-main-nav');
+const dropDownBtn = document.querySelector('.mobile');
+const dropDownContent = document.querySelector('.mobile-content');
 
-mobileNavBtn.addEventListener("click", displayMobileNav )
+mobileNavBtn.addEventListener('click', displayMobileNav);
+dropDownBtn.addEventListener('click', dropdownTest);
 
 function displayMobileNav() {
-    mobileMainNav.classList.toggle("display-flex")
+	mobileMainNav.classList.toggle('display-flex');
+}
+
+function dropdownTest(event) {
+	dropDownContent.classList.toggle('display-block');
 }
