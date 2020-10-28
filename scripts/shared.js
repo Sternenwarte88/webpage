71dcc7ce88082d6e018a2a6d9e1400c196d203b6
Wusabinga <s.loecher.slc@gmail.com>
2020-10-28 10:35:45 +0100

    Inplemented disabled button inplemented close button on mobile nav menu

--- scripts/shared.js
+++ scripts/shared.js
@@ -2,6 +2,7 @@ const mobileNavBtn = document.querySelector('.fa-bars');
 const mobileMainNav = document.querySelector('.mobile-main-nav');
 const dropDownBtn = document.querySelector('.mobile');
 const dropDownContent = document.querySelector('.mobile-content');
+const dropDownCross = document.getElementById('nav_close');
 
 function displayMobileNav() {
 	mobileMainNav.classList.toggle('display-flex');
@@ -11,5 +12,20 @@ function dropdownTest() {
 	dropDownContent.classList.toggle('display-block');
 }
 
+function navClose() {
+	if (dropDownContent.classList.contains('display-block')) {
+		mobileMainNav.classList.remove('display-flex');
+		dropDownContent.classList.remove('display-block');
+	} else if (
+		!dropDownContent.classList.contains('displayblock') &&
+		mobileMainNav.classList.contains('display-flex')
+	) {
+		mobileMainNav.classList.remove('display-flex');
+	} else {
+		return;
+	}
+}
+
 mobileNavBtn.addEventListener('click', displayMobileNav);
 dropDownBtn.addEventListener('click', dropdownTest);
+dropDownCross.addEventListener('click', navClose);
