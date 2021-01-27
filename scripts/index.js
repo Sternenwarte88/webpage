b97d2a35900029a4125625feb2dd2c88e13159b2
Wusabinga <s.loecher.slc@gmail.com>
2021-01-27 16:48:46 +0100

    0000008: [Design] Get skills aligned (Wusabinga)  0000005: [Bugs] Fix one Time only modal (Wusabinga)  0000020: [Design] Add Bug Tracker link (Wusabinga)  0000003: [Design] Get rid of Survey (Wusabinga)  0000004: [Design] add Dev-Labs link (Wusabinga)

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,35 @@
 const modalBtn = document.querySelector('.modal__button');
-const modalBackdrop = document.querySelector('.modal__backdrop')
+const modalBackdrop = document.querySelector('.modal__backdrop');
+
+const getCookie = function getCookie(name) {
+	const nameValue = name + '=';
+	const decodedCookie = decodeURIComponent(document.cookie);
+	const splittedCookies = decodedCookie.split(';');
+	for (let i = 0; i < splittedCookies.length; i++) {
+		let cookie = splittedCookies[i];
+		while (cookie.charAt(0) === ' ') {
+			cookie = cookie.substr(1);
+		}
+		if (cookie.indexOf(name) === 0) {
+			return cookie.substring(nameValue.length, cookie.length);
+		}
+	}
+	return '';
+};
+
+const checkCookie = function checkCookie(name) {
+	let firstVisitCookie = getCookie(name);
+	if (firstVisitCookie === 'false') {
+		modalBackdrop.style.display = 'none';
+		console.log(false);
+	} else {
+		document.cookie = 'firstVisit = false';
+		console.log(true);
+	}
+};
+
+checkCookie('firstVisit');
 
 modalBtn.addEventListener('click', () => {
-    modalBackdrop.style.display = "none";
-})
+	modalBackdrop.style.display = 'none';
+});
